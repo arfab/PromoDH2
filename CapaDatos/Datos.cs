6bbb1f5ec21b38d968edba71a937bb85e8ff39c3
Ariel Fabian <arfablumia@hotmail.com>
2020-12-11 10:53:17 -0300

    Varios

---
+++ CapaDatos/Datos.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace PromoDH.CapaDatos
+{
+    public class Datos  
+    {
+
+        static readonly string strConnectionString = Tools.GetConnectionString();
+
+
+        public static List<Models.Premio> ObtenerGanadores()
+        {
+            List<Models.Premio> lPremios = new List<Models.Premio>();
+
+            using (IDbConnection con = new SqlConnection(strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                lPremios = con.Query<Models.Premio>("spObtenerGanadores").ToList();
+            }
+
+            return lPremios;
+        }
+
+        public static List<Models.Sembrado> ObtenerSembrado()
+        {
+            List<Models.Sembrado> lSembrado = new List<Models.Sembrado>();
+
+            using (IDbConnection con = new SqlConnection(strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                lSembrado = con.Query<Models.Sembrado>("spObtenerPremios").ToList();
+            }
+
+            return lSembrado;
+        }
+
+    }
+}
---
+++ Tools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PromoDH
+{
+    public class Tools
+    {
+        public static string GetConnectionString(string name = "DefaultConnection")
+        {
+            return "Data Source=190.210.162.183;Initial Catalog=PromoTopline2020;Persist Security Info=True;User ID=PromoTopline2020;Password=PromoTopline2020";
+        }
+
+    }
+}
