ac9e7c567087de8f6296a042e9b9c40a3e72445c
Ariel Fabian <arfablumia@hotmail.com>
2020-12-29 11:12:23 -0300

    Google recaptcha no anda

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GoogleReCaptcha.V3;
+using GoogleReCaptcha.V3.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -33,6 +35,10 @@ namespace PromoDH
             });
 
             services.AddControllersWithViews();
+
+            services.AddHttpClient<ICaptchaValidator, GoogleReCaptchaValidator>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
