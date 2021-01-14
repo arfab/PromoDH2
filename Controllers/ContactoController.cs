461d4d6b27ee896e3a335279093de27ff8a398f6
Ariel Fabian <arfablumia@hotmail.com>
2021-01-14 08:05:29 -0300

    Acentos

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -160,30 +160,30 @@ namespace PromoDH.Controllers
             string sRet = "";
 
             if (contacto.Nombre.Trim().Length > 50) return "El Nombre no debe exceder los 50 caracteres";
-            if (contacto.Nombre.Trim().Length == 0) return "Algunos campos estan incompletos.";
+            if (contacto.Nombre.Trim().Length == 0) return "Algunos campos están incompletos.";
 
             if (contacto.Apellido.Trim().Length > 50) return "El Apellido no debe exceder los 50 caracteres";
-            if (contacto.Apellido.Trim().Length == 0) return "Algunos campos estan incompletos.";
+            if (contacto.Apellido.Trim().Length == 0) return "Algunos campos están incompletos.";
 
-            if (contacto.Dni.Trim().Length == 0) return "Algunos campos estan incompletos.";
-            if (!int.TryParse(contacto.Dni, out _)) return "El DNI no es valido";
-            if (contacto.Dni.Trim() == "") return "Algunos campos estan incompletos.";
-            if (contacto.Dni.Length < 7) return "El DNI no es valido";
-            if (contacto.Dni.Length > 8) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("00")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("111111")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("222222")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("333333")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("444444")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("555555")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("666666")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("777777")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("888888")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("999999")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("123456")) return "El DNI no es valido";
-            if (contacto.Dni.StartsWith("987654")) return "El DNI no es valido";
+            if (contacto.Dni.Trim().Length == 0) return "Algunos campos están incompletos.";
+            if (!int.TryParse(contacto.Dni, out _)) return "El DNI no es válido";
+            if (contacto.Dni.Trim() == "") return "Algunos campos están incompletos.";
+            if (contacto.Dni.Length < 7) return "El DNI no es válido";
+            if (contacto.Dni.Length > 8) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("00")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("111111")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("222222")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("333333")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("444444")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("555555")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("666666")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("777777")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("888888")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("999999")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("123456")) return "El DNI no es válido";
+            if (contacto.Dni.StartsWith("987654")) return "El DNI no es válido";
 
-            if (contacto.Consulta.Trim().Length == 0) return "Algunos campos estan incompletos.";
+            if (contacto.Consulta.Trim().Length == 0) return "Algunos campos están incompletos.";
 
 
             return sRet;
